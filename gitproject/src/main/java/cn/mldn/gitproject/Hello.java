a98099d88b6ebce92b65cf02b36a2a857f23dff4
wff <870199809@qq.com>
2019-07-02 19:13:45 +0800

    second send

--- gitproject/src/main/java/cn/mldn/gitproject/Hello.java
+++ gitproject/src/main/java/cn/mldn/gitproject/Hello.java
@@ -2,6 +2,6 @@ package cn.mldn.gitproject;
 
 public class Hello {
 	public static void main(String[] args) {
-		System.out.println("hello world");
+		System.out.println("hello world oh yes");
 	}
 }
--- gitproject/src/main/webapp/index.jsp
+++ gitproject/src/main/webapp/index.jsp
@@ -1,5 +1,5 @@
 <html>
 <body>
-<h2>Hello World!</h2>
+<h2>Hello World!wwww</h2>
 </body>
 </html>
