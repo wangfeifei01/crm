7320416ddcbd524674beb09d9206eb508f72b0cc
wangfeifei01 <52348569+wangfeifei01@users.noreply.github.com>
2019-07-02 19:16:33 +0800

    Update Hello.java

--- gitproject/src/main/java/cn/mldn/gitproject/Hello.java
+++ gitproject/src/main/java/cn/mldn/gitproject/Hello.java
@@ -3,5 +3,6 @@ package cn.mldn.gitproject;
 public class Hello {
 	public static void main(String[] args) {
 		System.out.println("hello world oh yes");
+		System.out.println("hello world oh yes----------------");
 	}
 }
