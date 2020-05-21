f2a0ea9d01a602b23fc3e61fd2e3fa21e7cd7129
wff <870199809@qq.com>
2020-05-21 10:52:16 +0800

    aa master origin

--- gitproject/src/main/java/cn/mldn/gitproject/Hello.java
+++ gitproject/src/main/java/cn/mldn/gitproject/Hello.java
@@ -3,6 +3,6 @@ package cn.mldn.gitproject;
 public class Hello {
 	public static void main(String[] args) {
 		System.out.println("hello world oh yes");
-		System.out.println("hello world oh yes-------再次提交---------");
+		System.out.println("hello world oh yes-----aa--再次提交---------");
 	}
 }
