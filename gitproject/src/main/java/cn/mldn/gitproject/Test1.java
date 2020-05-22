f9262f65ddf086557537ff1f12516d6d48962ae1
Bob@git.com <870199809@qq.com>
2020-05-22 10:21:20 +0800

    xintianjia wangff02 origin

--- gitproject/src/main/java/cn/mldn/gitproject/Test1.java
+++ gitproject/src/main/java/cn/mldn/gitproject/Test1.java
@@ -4,5 +4,6 @@ public class Test1 {
     public static void main(String[] args) {
         System.out.println("这是test1--------");
         System.out.println("再次修改++次修改+++++zailai+++111++++++++");
+        System.out.println("新添加的----------------");
     }
 }
