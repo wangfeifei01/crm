46fc522d8f7e4699d83f56faea303399ce76c7ef
Bob@git.com <870199809@qq.com>
2020-05-23 09:29:43 +0800

    xintianjia wangff02 origin

--- gitproject/src/main/java/cn/mldn/gitproject/IgnoreTest.java
+++ gitproject/src/main/java/cn/mldn/gitproject/IgnoreTest.java
@@ -2,6 +2,6 @@ package cn.mldn.gitproject;
 
 public class IgnoreTest {
     public static void main(String[] args) {
-        System.out.println("忽略---------------");
+        System.out.println("忽略------11111---------");
     }
 }
---
+++ gitproject/src/main/java/cn/mldn/gitproject/TestTT.java
@@ -0,0 +1,7 @@
+package cn.mldn.gitproject;
+
+public class TestTT {
+    public static void main(String[] args) {
+        System.out.println("-----------");
+    }
+}
